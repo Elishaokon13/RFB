1a6291cda1e19b490ebca7520fb6d508e9563055
OxBryte <silascyrax@gmail.com>
2025-07-20 03:01:22 +0100

    Refactor App component to include Layout wrapper for improved structure. Simplify Index page by removing Header and Sidebar, focusing on displaying the TokenTable directly.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,17 +1,7 @@
-import { Header } from "@/components/Header";
-import { Sidebar } from "@/components/Sidebar";
 import { TokenTable } from "@/components/TokenTable";
 
 const Index = () => {
-  return (
-    <div className="min-h-screen bg-background flex relative">
-      <Sidebar />
-      <div className="flex-1 flex flex-col">
-        <Header />
-        <TokenTable />
-      </div>
-    </div>
-  );
+  return <TokenTable />;
 };
 
 export default Index;
