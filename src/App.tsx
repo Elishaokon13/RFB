016789deb6a5d416b3f408e2ff568fea8e86fa64
Elishaokon13 <eliscodavid@gmail.com>
2025-07-20 23:14:49 +0100

    Add Creators route and update Sidebar navigation
    - Introduced a new route for the Creators page in App.tsx.
    - Enhanced Sidebar component to navigate to the new Creators page using useNavigate from react-router-dom.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import TokenDetails from "./pages/TokenDetails";
+import Creators from "./pages/Creators";
 import NotFound from "./pages/NotFound";
 import Layout from "./components/Layout";
 
@@ -20,6 +21,7 @@ const App = () => (
           <Route element={<Layout />}>
             <Route path="/" element={<Index />} />
             <Route path="/token/:address" element={<TokenDetails />} />
+            <Route path="/creators" element={<Creators />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Route>
